f97c735f23ae0f4c03a34b18e237b3ed3c1eceb3
Atomicend2 <mrmaize2@gmail.com>
2025-07-05 18:30:11 +0100

    Update index.js

--- index.js
+++ index.js
@@ -1,12 +1,2 @@
 const { startBot } = require('./lib/bot');
 startBot();
-// ⚡ Dummy Express server for Render port binding
-const express = require('express');
-const app = express();
-const PORT = process.env.PORT || 3000;
-
-app.get('/', (req, res) => res.send('🤖 ATOMICEND-XMD is alive!'));
-
-app.listen(PORT, () => {
-  console.log(`🌐 Web server running on port ${PORT}`);
-});
